7af1e5e549e4aac1a2ee78db5a5a75479a668170
4LEAF-LSH <4leaf.lsh@gmail.com>
2021-12-19 20:28:50 +0900

    modily

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const dotenv = require("dotenv");
 dotenv.config();
 const morgan = require(`morgan`);
 const path = require("path");
+const mysql2 = require("mysql2");
 
 const app = express();
 
 const PORT = 4000;
+
+const db = mysql2.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
+
+console.log(db);
 
 app.set("view engine", "pug");
 app.use(morgan("dev"));
@@ -14,8 +25,27 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// app.get("/", (req, res) => {
+//   res.render("screens/main");
+// });
+
 app.get("/", (req, res) => {
-  res.render("screens/main");
+  const selectQuery = `
+  SELECT  id,
+          name,
+          title
+  FROM 	  volunteer
+  `;
+
+  try {
+    db.query(selectQuery, (error, rows) => {
+      console.log(rows, "rows");
+      res.render("screens/main", { mList: rows });
+    });
+  } catch (error) {
+    console.log(error);
+    console.log("error");
+  }
 });
 
 app.get("/besiness", (req, res) => {
