6863e25091b4cfbd1a3b46bb094c23b30480fd7c
jungseokman <seokman.jung@gmail.com>
2021-12-24 11:40:51 +0900

    sponsor correction

---
+++ public/js/sponsor.js
@@ -0,0 +1,15 @@
+const account = document.getElementById("sponsor__account");
+const newDiv = document.createElement("div");
+const btn = document.getElementById("sponsor__btn");
+
+account.appendChild(newDiv);
+
+
+function handleClick() {
+
+    newDiv.innerHTML = "sadasd";
+}
+
+btn.addEventListener("click", handleClick);
+        
+
